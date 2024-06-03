645746375a436384b9b36b9d3b890f9c99345914
Ondřej Honus <honusondrej@gmail.com>
2024-06-03 08:44:34 +0200

    different ir codes

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,28 @@
 #include <Arduino.h>
+
 #include <IRremote.hpp>
 
 #define IR_MODULE_PIN 12 // This is the port your connect your IR reciever OUTPUT
-#define BTN_RED 19365 // Get the raw code from your controller and set your own code for the red button
-#define BTN_GREEN 19366 // Do the same thing as the red button, but for the green button
-#define BTN_BLUE 19364 // -||-
-#define RESET 149 // Set your code for a reset button on your controller
+#define BTN_RED 3091726080 // Get the raw code from your controller and set your own code for the red button
+#define BTN_GREEN 3158572800 // Do the same thing as the red button, but for the green button
+#define BTN_BLUE 4127850240 // -||-
+#define RESET 4161273600 // Set your code for a reset button on your controller
 
 int rgbs[] = {6, 5, 3};  // Red D3, Green D5, Blue D6
 const int count = 3;
 int values[count];
 
 unsigned long codes[] = {
-    137,  // Zero
-    128,  // One
-    129,  // Two
-    130,  // Three
-    131,  // Four
-    132,  // Five
-    133,  // Six
-    134,  // Seven
-    135,  // Eight
-    136   // Nine
+    3910598400,  // Zero
+    4077715200,  // One
+    3877175040,  // Two
+    2707357440,  // Three
+    4144561920,  // Four
+    3810328320,  // Five
+    2774204160,  // Six
+    3175284480,  // Seven
+    2907897600,  // Eight
+    3041591040   // Nine
 };
 
 String displayed_number = "";
